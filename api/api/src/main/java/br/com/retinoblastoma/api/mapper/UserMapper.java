6c838f3173d1838136e53854a95b55b7abe21c4c
Guilherme <guilhermesantos.srs@gmail.com>
2025-07-09 23:12:43 -0300

    Adicionado Mapper para User.

---
+++ api/api/src/main/java/br/com/retinoblastoma/api/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package br.com.retinoblastoma.api.mapper;
+
+import br.com.retinoblastoma.model.dto.UserDto;
+import br.com.retinoblastoma.model.entities.User;
+
+public class UserMapper {
+
+	private UserMapper() {
+	}
+
+	public static User toUser(UserDto userDto) {
+		return User.builder().id(userDto.getId()).name(userDto.getName()).email(userDto.getEmail())
+				.password(userDto.getPassword()).role(userDto.getRole()).build();
+	}
+
+	public static UserDto toUserDto(User user) {
+		return UserDto.builder().id(user.getId()).name(user.getName()).email(user.getEmail()).role(user.getRole())
+				.build();
+	}
+
+}
