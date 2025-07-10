c0a29850b26408296c0aa7b9a5abd6f34024858a
Guilherme <guilhermesantos.srs@gmail.com>
2025-07-09 23:14:42 -0300

    Criando um construtor para Doctor.

--- model/model/src/main/java/br/com/retinoblastoma/model/entities/Doctor.java
+++ model/model/src/main/java/br/com/retinoblastoma/model/entities/Doctor.java
@@ -5,6 +5,11 @@ import jakarta.persistence.Entity;
 @Entity
 public class Doctor extends User {
 
+	Doctor(Long id, String name, String email, String password, String role) {
+		super(id, name, email, password, role);
+		// TODO Auto-generated constructor stub
+	}
+
 	private String crm;
 	private String specialty;
 
