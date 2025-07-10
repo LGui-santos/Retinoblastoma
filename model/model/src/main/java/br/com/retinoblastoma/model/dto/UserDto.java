ad87d5cf26004ff17046d5338a7443a63fc06266
Guilherme <guilhermesantos.srs@gmail.com>
2025-07-09 23:09:30 -0300

    Adicionando UserDto.

---
+++ model/model/src/main/java/br/com/retinoblastoma/model/dto/UserDto.java
@@ -0,0 +1,18 @@
+package br.com.retinoblastoma.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private String role;
+}
