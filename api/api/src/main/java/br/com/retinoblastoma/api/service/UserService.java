b38b5c2b09b9fbe6544bba6e44ac0a8b0b9f5ec2
Guilherme <guilhermesantos.srs@gmail.com>
2025-07-09 23:11:56 -0300

    Adicionando UserService.

---
+++ api/api/src/main/java/br/com/retinoblastoma/api/service/UserService.java
@@ -0,0 +1,29 @@
+package br.com.retinoblastoma.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.retinoblastoma.api.mapper.UserMapper;
+import br.com.retinoblastoma.api.repository.UserRepository;
+import br.com.retinoblastoma.model.dto.UserDto;
+import br.com.retinoblastoma.model.entities.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private final UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public UserDto saveUser(UserDto userDto) {
+
+		userDto.setRole("Usuário");
+		User user = UserMapper.toUser(userDto);
+		User userSaved = userRepository.save(user);
+		return UserMapper.toUserDto(userSaved);
+	}
+
+}
